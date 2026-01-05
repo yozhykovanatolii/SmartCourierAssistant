a3d73bd545e686405f4a572868fbf9e7d9e27065
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2026-01-05 13:42:59 +0200

    Updating AuthRepository and connect AuthRepository to LoginCubit

--- lib/presentation/bloc/login/login_cubit.dart
+++ lib/presentation/bloc/login/login_cubit.dart
@@ -1,7 +1,11 @@
 import 'package:bloc/bloc.dart';
+import 'package:smart_courier_assistant/core/exception/auth/login_exception.dart';
+import 'package:smart_courier_assistant/data/repository/auth_repository.dart';
 import 'package:smart_courier_assistant/presentation/bloc/login/login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
+  final AuthRepository _authRepository = AuthRepository();
+
   LoginCubit() : super(LoginState.initial());
 
   void setLoginEmail(String email) {
@@ -15,5 +19,18 @@ class LoginCubit extends Cubit<LoginState> {
   Future<void> signInUser() async {
     if (state.emailError != null || state.passwordError != null) return;
     emit(state.copyWith(formStatus: FormStatus.loading));
+    try {
+      await _authRepository.signInUser(state.email, state.password);
+      emit(state.copyWith(formStatus: FormStatus.success));
+    } on LoginException catch (exception) {
+      emit(
+        state.copyWith(
+          errorMessage: exception.message,
+          formStatus: FormStatus.failure,
+        ),
+      );
+    } finally {
+      emit(state.copyWith(formStatus: FormStatus.initial));
+    }
   }
 }
