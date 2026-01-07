4dd2d989d7ec2abf7d2d9c971351dec0712ce8d3
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2026-01-07 13:18:59 +0200

    Connecting Supabase to the project

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,18 @@ import 'package:smart_courier_assistant/presentation/bloc/forgot_password/forgot
 import 'package:smart_courier_assistant/presentation/bloc/login/login_cubit.dart';
 import 'package:smart_courier_assistant/presentation/bloc/register/register_cubit.dart';
 import 'package:smart_courier_assistant/presentation/page/splash/splash_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await Supabase.initialize(
+    url: 'https://xwgraskemxbhjtgqrjxn.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3Z3Jhc2tlbXhiaGp0Z3FyanhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNjI0MDIsImV4cCI6MjA3MzkzODQwMn0.8N39PRYkuu8vS0j69ucNSNblXDct7NBT7IrVqBpFK8c',
   );
   runApp(
     MultiBlocProvider(
