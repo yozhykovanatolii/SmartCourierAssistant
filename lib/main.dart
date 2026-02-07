701f02a234c165e7ec25e5b10e48d014fedeb990
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2026-02-07 16:22:26 +0200

    Creating file .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smart_courier_assistant/core/theme/app_theme.dart';
